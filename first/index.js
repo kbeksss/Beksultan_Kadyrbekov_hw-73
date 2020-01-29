37304db25dcf045be65659745ce1c46d460ca5f0
kbeksss <kbeksss@outlook.com>
2020-01-30 03:01:14 +0600

    Started doing the second task

--- first/index.js
+++ first/index.js
@@ -4,9 +4,9 @@ const app = express();
 const port = 8000;
 
 app.get('/', (req, res) => {
-    res.header('Content-Type', 'text/html');
     res.send('<h2>Please write a word after the slash</h2>')
 });
+
 app.get('/:word', (req, res) => {
     res.send(`<h2>${req.params.word}</h2>`);
 });
